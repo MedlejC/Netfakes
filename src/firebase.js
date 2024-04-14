88e90fc27f4c2800e82c9a2193bbacb844dc2884
MedlejC <charbel.medlej@lau.edu>
2024-04-14 17:35:24 +0300

    Initialized Firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2ZpItinFsYBO5D2GgHpcK0mO59tQj9eY",
+    authDomain: "netfakes-d4679.firebaseapp.com",
+    projectId: "netfakes-d4679",
+    storageBucket: "netfakes-d4679.appspot.com",
+    messagingSenderId: "455611584682",
+    appId: "1:455611584682:web:7e2e6f32fccd84326700d3"
+  };
