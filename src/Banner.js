557719637eee2a77840e50892c984977fdacf9a7
MedlejC <charbel.medlej@lau.edu>
2024-04-14 23:39:01 +0300

    Implemented Banner's title, description and image from TMDB

--- src/Banner.js
+++ src/Banner.js
@@ -47,12 +47,14 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+            {movie?.name || movie?.title || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{truncate(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar porttitor placerat. Ut enim nunc, hendrerit a congue sit amet, commodo at justo. Ut rutrum tellus sit amet fermentum hendrerit. Mauris molestie dolor eu neque consequat, sit amet mollis mauris feugiat. Mauris feugiat, sem vel posuere elementum, purus libero blandit leo, et pretium nisi arcu sollicitudin risus. Duis efficitur tortor nec sem consectetur, id fermentum mi malesuada. Mauris vel pulvinar neque. Praesent lorem purus, pellentesque nec rhoncus at, aliquam a diam. Suspendisse laoreet sapien orci, quis bibendum lectus dapibus sed. Suspendisse potenti.`, 150)}</h1>
+        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
       </div>
 
 
