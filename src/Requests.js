31b469fca726f5ad94e05f27b87c6cb4ad682acd
MedlejC <charbel.medlej@lau.edu>
2024-04-14 22:28:06 +0300

    Implemented TMDB

---
+++ src/Requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "465e74fcad3250ba8db072b9a5c51657";
+
+const requests = {
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+};
+
+export default requests;
+
+/// https://api.themoviedb.org/3/discover/movie?api_key=465e74fcad3250ba8db072b9a5c51657&with_genres=99
